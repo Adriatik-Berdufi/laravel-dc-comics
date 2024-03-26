417e620ca34e168c4bbfce685ddd5d1949d6420b
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-03-26 20:11:05 +0100

    Add seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
 class ComicSeeder extends Seeder
 {
     /**
@@ -12,8 +13,19 @@ class ComicSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+            $comic = new Comic();
+            $comic->title = $faker->name();
+            $comic->description = $faker->paragraph();
+            $comic->thumb = $faker->imageUrl(640, 480, 'comic', true);
+            $comic->price = $faker->randomFloat(2,9, 30);
+            $comic->series = $faker->word();
+            $comic->sale_date = $faker->dateTimeBetween('-3 years', '-1 day');
+            $comic->type= $faker->word();
+            $comic->save();
+
+        }
     }
 }
